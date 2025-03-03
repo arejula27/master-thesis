b8ece44eff0c766b607f69cdfbd9a98c5f227ad0
Íñigo Aréjula Aísa <arejula27@gmail.com>
2025-03-03 12:26:01 +0100

    init

---
+++ change_schema.py
@@ -0,0 +1,77 @@
+import os
+import pyspark
+from delta import *
+from delta.tables import *
+import threading
+
+TABLE_PATH = "delta-table"
+
+
+def change_schema(old_column, new_column):
+    # Change the schema of the delta table
+    df = spark.read.format("delta").load(TABLE_PATH)
+    df = df.withColumnRenamed(old_column, new_column)
+    try:
+        df.write.format("delta").mode("overwrite").option(
+            "overwriteSchema", "true").save(TABLE_PATH)
+    except Exception as e:
+        print("=== Error ===")
+        print(e)
+        print("==============")
+
+
+builder = pyspark.sql.SparkSession.builder.appName("MyApp") \
+    .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
+    .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
+
+
+spark = configure_spark_with_delta_pip(builder).getOrCreate()
+
+
+# delete the current delta table
+os.system(f"rm -rf {TABLE_PATH}")
+
+# create a new delta TABLE
+data = [("Alice", 34), ("Bob", 56), ("Charlie", 45)]
+columns = ["name", "age"]
+df = spark.createDataFrame(data, columns)
+df.write.format("delta").save(TABLE_PATH)
+
+# read the delta table
+df = spark.read.format("delta").load(TABLE_PATH)
+df.show()
+
+
+thread1 = threading.Thread(target=change_schema, args=("name", "first_name"))
+thread2 = threading.Thread(target=change_schema, args=("name", "full_name"))
+
+# Start both threads
+thread1.start()
+thread2.start()
+
+# Wait for both threads to finish
+thread1.join()
+thread2.join()
+
+# read the delta TABLE_PATH
+df = spark.read.format("delta").load(TABLE_PATH)
+df.show()
+
+# get column names
+columns = df.columns
+
+thread1 = threading.Thread(
+    target=change_schema, args=(columns[0], "user_name"))
+thread2 = threading.Thread(target=change_schema, args=(columns[1], "score"))
+
+# Start both threads
+thread1.start()
+thread2.start()
+
+# Wait for both threads to finish
+thread1.join()
+thread2.join()
+
+# read the delta TABLE_PATH
+df = spark.read.format("delta").load(TABLE_PATH)
+df.show()
