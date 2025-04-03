dd88f574016a161ab0d25d84c14c4b352101f2c4
Íñigo Aréjula Aísa <arejula27@gmail.com>
2025-04-03 17:13:53 +0200

    small improvements

--- bench.py
+++ bench.py
@@ -16,12 +16,12 @@ from typing import Callable, Tuple
 TABLE_PATH = "delta-table-bench"
 
 # Number of seconds to run the test
-SECONDS = 10
+ITERATIONS = 10
 # Concurrent readers and writers per second
 NUM_READERS = 5
 NUM_WRITERS = 3
 # Max concurrent threads
-MAX_THREADS = NUM_READERS + NUM_WRITERS
+MAX_THREADS = NUM_READERS + NUM_WRITERS+1
 
 
 def random_string(length=10):
@@ -36,10 +36,18 @@ def read_delta_table():
     df.show()
 
 
-def append_delta_table():
+def append_delta_row():
     # Append a new row to the delta table
     data = [(random_string(),)]
     columns = ["col1"]
+    df = spark.createDataFrame(data, columns)
+    df.write.format("delta").mode("append").save(TABLE_PATH)
+
+
+def change_schema_and_append_delta_row():
+    # Append a new row to the delta table
+    data = [(random_string(), random_string())]
+    columns = ["col1", random_string()]
     df = spark.createDataFrame(data, columns)
     df.write.format("delta").mode("append").save(TABLE_PATH)
 
@@ -76,8 +84,8 @@ def worker(tasks: queue.Queue[Callable[[], None]],
             tasks.task_done()
 
 
-def print_stats(operation_count):
-    print("=== Threads finished ===")
+def print_stats(operation_task_per_iter, operation_count):
+    print("=== Statistics ===")
     print(f"Total number of threads: {MAX_THREADS}")
 
     total_success = sum(counts['success']
@@ -85,9 +93,16 @@ def print_stats(operation_count):
     total_failure = sum(counts['failure']
                         for counts in operation_count.values())
     print(f"Total number of operations: {total_success + total_failure}")
-    print(f"Total number of successful operations: {total_success}")
-    print(f"Total number of failed operations: {total_failure}")
+    print(f"Total number of iterations: {ITERATIONS}")
+    for operation, number in operation_task_per_iter:
+        print(f"\tNumber of {
+              operation.__name__} per second called: {number}")
+
     print("=== Operation Results ===")
+    print(f"Total number of successful operations: {total_success}")
+
+    print(f"Total number of failed operations: {total_failure}")
+
     for operation, counts in operation_count.items():
         print(f"Operation: {operation}")
         print(f"\tSuccessful operations: {counts['success']}")
@@ -100,7 +115,9 @@ def main():
     results = queue.Queue()
 
     operations = [(read_delta_table, NUM_READERS),
-                  (append_delta_table, NUM_WRITERS)]
+                  (change_schema_and_append_delta_row, 1),
+                  (append_delta_row, NUM_WRITERS),
+                  ]
     operation_count = defaultdict(lambda: {"success": 0, "failure": 0})
     # Create a thread pool executor
     executor = ThreadPoolExecutor(max_workers=MAX_THREADS)
@@ -108,14 +125,12 @@ def main():
     for _ in range(MAX_THREADS):
         executor.submit(worker, tasks, results)
 
-    operations = [(read_delta_table, NUM_READERS),
-                  (append_delta_table, NUM_WRITERS)]
     # capture the stdout and stderr
     old_stdout = sys.stdout
     old_stderr = sys.stderr
     sys.stdout = io.StringIO()
     sys.stderr = io.StringIO()
-    for _ in range(SECONDS):
+    for _ in range(ITERATIONS):
         for operation, count in operations:
             for _ in range(count):
                 tasks.put(operation, block=False)
@@ -126,8 +141,6 @@ def main():
     # Restore stdout and stderr
     sys.stdout = old_stdout
     sys.stderr = old_stderr
-
-    print("=== All tasks done ===")
 
     # Count the number of successful and failed operations
     operation_count = defaultdict(lambda: {"success": 0, "failure": 0})
@@ -141,7 +154,7 @@ def main():
             operation_count[operation]["failure"] += 1
 
     # Print the results
-    print_stats(operation_count)
+    print_stats(operations, operation_count)
 
     # Shutdown the executor
     executor.shutdown(wait=True)
