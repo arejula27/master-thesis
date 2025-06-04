664c46f6ffaa4471239a552f7d31c376be916d64
Íñigo Aréjula Aísa <arejula27@gmail.com>
2025-06-04 16:29:12 +0200

    json experiment concurrent test done

--- exploration-experiments/write_json.py
+++ exploration-experiments/write_json.py
@@ -1,4 +1,5 @@
 import os
+import threading
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, when
 
@@ -14,6 +15,8 @@ spark = SparkSession.builder \
 
 # Path to input JSON and output Delta
 input_path = "data/nested.json"
+input_path_2 = "data/nested_2.json"
+input_path_3 = "data/nested_3.json"
 
 # Read the JSON file (automatically infers schema including nested structure)
 df = spark.read.option("multiline", "true").json(input_path)
@@ -37,3 +40,22 @@ df.write.format("delta").mode("overwrite").save(TABLE_PATH)
 # Read back and verify change
 df = spark.read.format("delta").load(TABLE_PATH)
 df.show(truncate=False)
+
+
+# Write concurrently both files to the same Delta TABLE_PATH
+
+df1 = spark.read.option("multiline", "true").json(input_path_3)
+df2 = spark.read.option("multiline", "true").json(input_path_2)
+thread1 = threading.Thread(target=df1.write.format(
+    "delta").mode("append").save, args=(TABLE_PATH,))
+thread2 = threading.Thread(target=df2.write.format(
+    "delta").mode("append").save, args=(TABLE_PATH,))
+thread1.start()
+thread2.start()
+
+
+thread1.join()
+thread2.join()
+# Read back and verify changes
+df_reader = spark.read.format("delta").load(TABLE_PATH)
+df_reader.show(truncate=False)
